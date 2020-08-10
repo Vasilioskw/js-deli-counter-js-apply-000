cb179913e7c2faedc4e69cd508ba6fbe53767765
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-10 15:41:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ return "There is nobody waiting to be served!"
 } else { 
   var name = x[0];
   x.splice(0,1);
-  return "Currently serving" + name + ".";
+  return "Currently serving " + name + ".";
   }
 }
