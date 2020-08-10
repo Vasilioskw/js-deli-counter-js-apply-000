3b49b7538441d57266087125b28be8e98bb2bcff
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-10 15:37:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(NewLine) {
     }
 }
 function nowserving(x) {
-if var x = 0 {
+if x.length === 0 {
 return "There is nobody waiting to be served!"
 } else { 
   var name = x{[0]
