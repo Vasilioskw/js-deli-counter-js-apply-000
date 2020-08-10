7a8c8e0965865877dc794c15732246e0da9bd95c
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-10 15:01:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function currentLine(NewLine) {
+  var line = []
+  if (NewLine.length === 0) {
+  return "The line is currently empty."
+  } else {
+  for(var i = 0; i<NewLine.length; i++) {
+    line += (i + 1) + ". " + NewLine[i] + ", " 
+  }
+    line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
+    }
+}
+
